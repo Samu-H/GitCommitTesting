c58dd818baea831e762e514cf95fd35b68aa6e8e
Samuel Hatler <93668004+Samu-H@users.noreply.github.com>
2021-11-05 16:24:32 +0000

    added "This is a second commit branching off from the first one"

--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("This commit has been added to the previous commit");
-
+    System.out.println("This is a second commit branching off from the first one");
   }
 }
