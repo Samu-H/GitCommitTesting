1945f98365028c0c8d58f01153cadc8decbd9789
Samuel Hatler <93668004+Samu-H@users.noreply.github.com>
2021-11-05 16:23:45 +0000

    Added "This commit has been added to the previous commit"

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("This is a new commit");
+    System.out.println("This commit has been added to the previous commit");
 
   }
 }
